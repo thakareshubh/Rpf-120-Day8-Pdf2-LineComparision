7ac37a12d46e98aacd104d5fb27f758f22d0e386
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-05 07:50:05 +0530

    Added Welcome msg code of line computition problem

---
+++ LineComputation/LineComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line comparision problem");
+        }
+    }
+}
